7c8b2e3503f2a7cf667afebac0a2405930e89219
eirblaze <eirblaze@live.jp>
2019-07-28 21:09:07 +0900

    pushコマンドをsourcetreeに合わせる

--- git-push.bat
+++ git-push.bat
@@ -1,3 +1,3 @@
 @powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
 
-git push
+git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks push -v --tags origin master:master
